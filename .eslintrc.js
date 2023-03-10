1dd305d066ee84525bd78ddd4da8a73deeb36cb8
Luke Griffith <lukegriffith@gmail.com>
2023-03-10 20:23:11 +0000

    update to tamagui

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,43 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  root: true,
-  extends: ["acme"], // uses the config in `packages/config/eslint`
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    tsconfigRootDir: __dirname,
-    project: [
-      "./tsconfig.json",
-      "./apps/*/tsconfig.json",
-      "./packages/*/tsconfig.json",
-    ],
-  },
-  settings: {
-    next: {
-      rootDir: ["apps/nextjs"],
-    },
-  },
-};
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"universe/native",
+		"universe/shared/typescript-analysis",
+		"prettier"
+	],
+	plugins: [],
+	rules: {
+		// We will use TypeScript's types for component props instead
+		"react/prop-types": "off",
 
-module.exports = config;
+		// Why would you want unused vars?
+		"@typescript-eslint/no-unused-vars": "warn",
+
+		// I suggest this setting for requiring return types on functions only where useful
+		"@typescript-eslint/explicit-function-return-type": "off",
+
+		// Includes .prettierrc.js rules
+		"prettier/prettier": [
+			"warn",
+			{
+				endOfLine: "auto",
+				printWidth: 80,
+				useTabs: true,
+				tabWidth: 2,
+				singleQuote: false,
+				bracketSameLine: false,
+				trailingComma: "none",
+				singleAttributePerLine: true
+			},
+			{ usePrettierrc: false }
+		]
+	},
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx", "*.d.ts"],
+			parserOptions: {
+				project: "./tsconfig.json"
+			}
+		}
+	]
+};
